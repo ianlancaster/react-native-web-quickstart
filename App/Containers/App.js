5874585135d160a2fdeb52b4a8860355fcfe9517
Ian Lancaster <ianclancaster@gmail.com>
2017-03-12 15:30:38 -0600

    remove more uneeded files

--- App/Containers/App.js
+++ App/Containers/App.js
@@ -2,13 +2,9 @@
 
 import React, { Component } from 'react'
 import { Provider } from 'react-redux'
-import '../I18n/I18n' // keep before root container
 import RootContainer from './RootContainer'
 import createStore from '../Redux'
-import applyConfigSettings from '../Config'
 
-// Apply config overrides
-applyConfigSettings()
 // create our store
 const store = createStore()
 
