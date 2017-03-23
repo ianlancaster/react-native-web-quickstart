ffb2788824fd38f8605ef481f594dde1674338a5
Ian Lancaster <ianclancaster@gmail.com>
2017-03-23 01:05:27 -0600

    Set up modular server route

---
+++ server/routes/helloWorldAsync/helloWorldAsync.test.js
@@ -0,0 +1,9 @@
+import { expect } from 'chai'
+import request from 'supertest'
+import app from '../../index'
+
+describe('Hello World Async Server Test', () => {
+  it('should pass', () => {
+    expect(true)
+  })
+})
